e9aee12cfe6ffd0a734340a87912d8c4182fa677
potados99 <potados99@gmail.com>
2021-01-20 14:01:15 +0900

    Initial

---
+++ main.test.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  watcher-node
+//
+//  Created by Potados on 2021/01/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
